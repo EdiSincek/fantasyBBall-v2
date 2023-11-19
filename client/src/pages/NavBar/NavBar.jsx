c781992b4bccb570302262e1b5f77a667d588bed
Edi Sincek <edi.sincek@gmail.com>
2023-11-19 14:49:27 +0100

    minor styling changes

--- client/src/pages/NavBar/NavBar.jsx
+++ client/src/pages/NavBar/NavBar.jsx
@@ -6,78 +6,78 @@ function NavBar() {
 
   return (
     <div className="NavBar">
-      <div class="page">
-        <nav class="page__menu menu">
-          <ul class="menu__list r-list">
-            <li class="menu__group">
-              <a href="/" class="menu__link r-link text-underlined">
+      <div className="page">
+        <nav className="page__menu menu">
+          <ul className="menu__list r-list">
+            <li className="menu__group">
+              <a href="/" className="menu__link r-link text-underlined">
                 HOME
               </a>
             </li>
-            <li class="menu__group">
-              <a href="/stats" class="menu__link r-link text-underlined">
+            <li className="menu__group">
+              <a href="/stats" className="menu__link r-link text-underlined">
                 TOTAL MATCHUP STATS
               </a>
             </li>
-            <div class="dropdown">
-              <button class="dropbtn">
-                <a href="/stats" class="menu__link r-link text-underlined">
+            <div className="dropdown">
+              <button className="dropbtn">
+                <a href="/stats" className="menu__link r-link text-underlined">
                   STATS
                 </a>
               </button>
-              <div class="dropdown-content">
+              <div className="dropdown-content">
                 <a
                   href="/stats/week/1"
-                  class="menu__link r-link text-underlined"
+                  className="menu__link r-link text-underlined"
                 >
                   Week 1
                 </a>
                 <a
                   href="/stats/week/2"
-                  class="menu__link r-link text-underlined"
+                  className="menu__link r-link text-underlined"
                 >
                   Week 2
                 </a>
                 <a
                   href="/stats/week/3"
-                  class="menu__link r-link text-underlined"
+                  className="menu__link r-link text-underlined"
                 >
                   Week 3
                 </a>
                 <a
                   href="/stats/week/4"
-                  class="menu__link r-link text-underlined"
+                  className="menu__link r-link text-underlined"
                 >
                   Week 4
                 </a>
               </div>
             </div>
 
-            <li class="menu__group">
+            <li className="menu__group">
               <a
                 href="/tradeAnalyzer"
-                class="menu__link r-link text-underlined"
+                className="menu__link r-link text-underlined"
               >
                 TRADE ANALYZER
               </a>
             </li>
-            <li class="menu__group">
+            <li className="menu__group">
               <a
                 href="/league/standings"
-                class="menu__link r-link text-underlined"
+                className="menu__link r-link text-underlined"
               >
                 STANDINGS
               </a>
             </li>
-            <li class="menu__group">
-              <a href="/player" class="menu__link r-link text-underlined">
+            <li className="menu__group">
+              <a href="/player" className="menu__link r-link text-underlined">
                 TEST PLAYER PAGE
               </a>
             </li>
           </ul>
         </nav>
-        <nav class="page__menu page__custom-settings menu">
-          <ul class="menu__list r-list"></ul>
+        <nav className="page__menu page__custom-settings menu">
+          <ul className="menu__list r-list"></ul>
         </nav>
       </div>
     </div>
